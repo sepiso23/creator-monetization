f649c868cbe171722b4eafdeb0ea96a299ae723d
zyambo <mightypz2@gmail.com>
2026-01-28 17:12:03 +0200

    chore(config): Remove cache settings

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -87,16 +87,6 @@ DATABASES = {
     )
 }
 
-CACHES = {
-    # Read os.environ['CACHE_URL'] and raises
-    # ImproperlyConfigured exception if not found.
-    #
-    # The cache() method is an alias for cache_url().
-    'default': env.cache(),
-
-    # read os.environ['REDIS_URL']
-    'redis': env.cache_url('REDIS_URL')
-}
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
