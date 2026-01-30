57f7ec521d2137633aa52e0bfacac48858a25cdf
zyambo <mightypz2@gmail.com>
2026-01-30 12:52:37 +0200

    feat: Add endpoints for creators public pages

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -5,6 +5,7 @@ from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, Spec
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/auth/', include('apps.customauth.urls')),
+    path('api/v1/creators/', include('apps.creators.urls')),
     # OpenAI Schema and Documentation
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
